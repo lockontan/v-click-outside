7c9c0feeaa08530ae37957096329a152dc88ea1f
tanzj <tanzj@keketour.com>
2019-03-08 15:48:22 +0800

    origin

---
+++ clickoutside.js
@@ -0,0 +1,46 @@
+const attr = 'clickoutside' + new Date().valueOf()
+const nodeList = []
+let id = 0
+
+const addEvent = (function () {
+	if (document.addEventListener) {
+		return function(el, event, handler) {
+      if (el && event && handler) {
+        el.addEventListener(event, handler, false)
+      }
+    }
+	} else if (document.attachEvent) {
+		return function(el, event, handler) {
+      if (el && event && handler) {
+        el.attachEvent('on' + event, handler)
+      }
+    }
+	}
+})()
+
+addEvent(document, 'click', (e) => {
+	nodeList.forEach(item => {
+		if (item.el.contains(e.target)) return
+		item.vnode.context[item.binding.expression]()
+	})
+})
+
+export default {
+  bind (el, binding, vnode) {
+  	el[attr] = ++id
+	  nodeList.push({
+	  	el,
+	  	binding,
+	  	vnode
+	  })
+	},
+
+	unbind (el, binding, vnode) {
+		for (let i = 0; i < nodeList.length; i++) {
+			if (nodeList[i].el.attr === el.attr) {
+        nodeList.splice(i, 1)
+        break
+      }
+		}
+	}
+}
