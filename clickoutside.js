431d695ff7e87867f64eaa3791ba1d88a49974fe
tanzj <tanzj@keketour.com>
2019-03-08 15:52:19 +0800

    origin

--- clickoutside.js
+++ clickoutside.js
@@ -3,26 +3,26 @@ const nodeList = []
 let id = 0
 
 const addEvent = (function () {
-	if (document.addEventListener) {
-		return function(el, event, handler) {
+  if (document.addEventListener) {
+    return function(el, event, handler) {
       if (el && event && handler) {
         el.addEventListener(event, handler, false)
       }
     }
-	} else if (document.attachEvent) {
-		return function(el, event, handler) {
+  } else if (document.attachEvent) {
+    return function(el, event, handler) {
       if (el && event && handler) {
         el.attachEvent('on' + event, handler)
       }
     }
-	}
+  }
 })()
 
 addEvent(document, 'click', (e) => {
-	nodeList.forEach(item => {
-		if (item.el.contains(e.target)) return
-		item.vnode.context[item.binding.expression]()
-	})
+  nodeList.forEach(item => {
+    if (item.el.contains(e.target)) return
+    item.vnode.context[item.binding.expression]()
+  })
 })
 
 /*
@@ -30,20 +30,20 @@ addEvent(document, 'click', (e) => {
 */
 export default {
   bind (el, binding, vnode) {
-  	el[attr] = ++id
-	  nodeList.push({
-	  	el,
-	  	binding,
-	  	vnode
-	  })
-	},
+    el[attr] = ++id
+    nodeList.push({
+      el,
+      binding,
+      vnode
+    })
+  },
 
-	unbind (el, binding, vnode) {
-		for (let i = 0; i < nodeList.length; i++) {
-			if (nodeList[i].el.attr === el.attr) {
+  unbind (el, binding, vnode) {
+    for (let i = 0; i < nodeList.length; i++) {
+      if (nodeList[i].el.attr === el.attr) {
         nodeList.splice(i, 1)
         break
       }
-		}
-	}
+    }
+  }
 }
